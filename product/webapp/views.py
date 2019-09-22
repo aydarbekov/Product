d86df6b343458fce85f177b6aa17f75c67cf413f
akyl <akyl.aydarbekov@gmail.com>
2019-09-22 23:39:27 +0600

    added main page and detal view

--- product/webapp/views.py
+++ product/webapp/views.py
@@ -1,1 +1,17 @@
-from django.db import models
+from webapp.models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+
+def index_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        products = Product.objects.exclude(amount=0).order_by('category', 'name')
+        return render(request, 'index.html', context={'products': products})
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', context={'product': product})
+
+
+
+
+
+
